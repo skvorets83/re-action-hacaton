4af27ae595955967997de316dbf5e208eaf47add
skvorets83 <st_skvor@mail.ru>
2026-09-10 13:58:45 +0300

    feat: working interactive ganntchart

---
+++ gantt-frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import GanttChart from './GanttChart'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-8">
+      <h1 className="text-3xl font-bold text-gray-800 mb-6">
+        Проект под контролем
+      </h1>
+
+      <GanttChart />
+    </div>
+  )
+}
+
+export default App
