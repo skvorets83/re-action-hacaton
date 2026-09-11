4496419985d6bc7a66f107973ba3061d340e40a0
Матвей <@DESKTOP-SMJCB5D>
2026-09-11 14:10:33 +0700

    sdfs

--- GanttManager.API/Program.cs
+++ GanttManager.API/Program.cs
@@ -63,7 +63,12 @@ builder.Services.AddCors(options =>
     });
 });
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+    });
+
 builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddSwaggerGen(c =>
